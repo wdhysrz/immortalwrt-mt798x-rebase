ce3a46248691e0b5d944ce58a3565e9a86f80897
chasey-dev <ellenyoung0912@gmail.com>
2026-07-02 18:00:30 +0800

    mtk: eqos: refactor for nftables and upstream debugfs layout
    eqos-mtk supports 31 flows, with qid 1-31 for UL, 32-62 for DL. qid 63 is reserved for GE PPD xmit, see 00a818f ("mediatek: hnat: bypass GE PPD carrier for ping-pong packets").
    
    tc may not work properly for now.
    
    Signed-off-by: chasey-dev <ellenyoung0912@gmail.com>

--- package/mtk/applications/luci-app-eqos-mtk/htdocs/luci-static/resources/view/eqos.js
+++ package/mtk/applications/luci-app-eqos-mtk/htdocs/luci-static/resources/view/eqos.js
@@ -1,8 +1,70 @@
 'use strict';
 'require form';
 'require network';
+'require tools.widgets as widgets';
 'require uci';
 'require view';
+
+function collectHostChoices(hosts) {
+	var choices = {
+		ip: [],
+		ip6: [],
+		mac: []
+	};
+
+	for (var mac in hosts) {
+		var ipaddrs = L.toArray(hosts[mac].ipaddrs || hosts[mac].ipv4);
+		var ip6addrs = L.toArray(hosts[mac].ip6addrs || hosts[mac].ipv6);
+		var name = hosts[mac].name;
+
+		for (var i = 0; i < ipaddrs.length; i++)
+			choices.ip.push([ ipaddrs[i], name ? '%s (%s)'.format(name, ipaddrs[i]) : ipaddrs[i] ]);
+
+		for (var j = 0; j < ip6addrs.length; j++)
+			choices.ip6.push([ ip6addrs[j], name ? '%s (%s)'.format(name, ip6addrs[j]) : ip6addrs[j] ]);
+
+		var hint = name || ipaddrs[0] || ip6addrs[0];
+		choices.mac.push([ mac, hint ? '%s (%s)'.format(mac, hint) : mac ]);
+	}
+
+	return choices;
+}
+
+function addChoices(option, choices) {
+	for (var i = 0; i < choices.length; i++)
+		option.value(choices[i][0], choices[i][1]);
+}
+
+function selectorValue(section_id) {
+	var selector = uci.get('eqos', section_id, 'selector');
+
+	if (selector === 'ip' || selector === 'ip6' || selector === 'mac')
+		return selector;
+
+	if (uci.get('eqos', section_id, 'ip6'))
+		return 'ip6';
+
+	if (uci.get('eqos', section_id, 'mac'))
+		return 'mac';
+
+	return 'ip';
+}
+
+function matchLabel(selector) {
+	if (selector === 'ip6')
+		return _('IPv6 address');
+
+	if (selector === 'mac')
+		return _('MAC address');
+
+	return _('IPv4 address');
+}
+
+function matchValue(section_id) {
+	var selector = selectorValue(section_id);
+
+	return uci.get('eqos', section_id, selector) || '';
+}
 
 return view.extend({
 	load: function() {
@@ -13,19 +75,24 @@ return view.extend({
 	},
 
 	render: function(data) {
+		var hosts = data[1] ? data[1].hosts || {} : {};
+		var hostChoices = collectHostChoices(hosts);
+		var queueTip = _('Slots 1-31 use HNAT HQoS; slots 32 and above use tc software shaping and require an IPv4 or IPv6 address.');
 		var m, s, o;
 
 		m = new form.Map('eqos', _('EQoS'),
- 			_('Network speed control service.(Compatiable with Mediatek HNAT)'));
- 			
-		s = m.section(form.NamedSection, 'config', 'eqos');
+			_('Network speed control service for MediaTek HNAT.'));
+
+		s = m.section(form.NamedSection, 'config', 'eqos', _('Settings'));
+		s.anonymous = true;
 
 		o = s.option(form.Flag, 'enabled', _('Enable'));
 		o.default = o.disabled;
 		o.rmempty = false;
-		
-		o = s.option(form.Flag, 'ipv6enabled', _('IPV6Enable'));
-		o.default = o.disabled;
+
+		o = s.option(widgets.DeviceSelect, 'dev', _('Controlled device'));
+		o.default = 'br-lan';
+		o.placeholder = 'br-lan';
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'download', _('Download speed (Mbit/s)'),
@@ -38,44 +105,127 @@ return view.extend({
 		o.datatype = 'and(uinteger,min(1))';
 		o.rmempty = false;
 
-		s = m.section(form.TableSection, 'device', _('Speed limit based on IP address(using unique comment less than 32 will enable hardware QOS)'));
+		s = m.section(form.GridSection, 'device', _('Device rules'));
 		s.addremove = true;
 		s.anonymous = true;
 		s.sortable = true;
+		s.handleAdd = function(ev) {
+			var section_id = uci.add('eqos', 'device');
 
-		o = s.option(form.Flag, 'enabled', _('Enable'));
+			uci.set('eqos', section_id, 'enabled', '1');
+			uci.set('eqos', section_id, 'selector', 'ip');
+			m.addedSection = section_id;
+
+			return this.renderMoreOptionsModal(section_id);
+		};
+
+		s.tab('general', _('General Settings'));
+
+		o = s.taboption('general', form.Flag, 'enabled', _('Enable'));
 		o.default = o.enabled;
-		
-		o = s.option(form.Value, 'ip', _('IPV4 address'));
-		o.datatype = 'ip4addr';
-		for (var i of Object.entries(data[1]?.hosts))
-			for (var v in i[1].ipaddrs)
-				if (i[1].ipaddrs[v]) {
-					var ip_addr = i[1].ipaddrs[v], ip_host = i[1].name;
-					o.value(ip_addr, ip_host ? String.format('%s (%s)', ip_host, ip_addr) : ip_addr)
-				}
 		o.rmempty = false;
-		
-		var hosts = data[1]?.hosts;
-		o = s.option(form.Value, 'mac', _('IPV6 host'));
+		o.editable = true;
+
+		o = s.taboption('general', form.Value, 'queue', _('Name'));
+		o.datatype = 'and(uinteger,min(1),max(65535))';
+		o.placeholder = '1';
+		o.rmempty = false;
+		o.editable = true;
+		o.renderWidget = function(section_id, option_index, cfgvalue) {
+			var node = form.Value.prototype.renderWidget.apply(this, arguments);
+			var input = node.querySelector('input') || node;
+
+			input.setAttribute('data-tooltip', queueTip);
+			input.setAttribute('title', queueTip);
+
+			return node;
+		};
+		o.validate = function(section_id, value) {
+			var rv = form.Value.prototype.validate.apply(this, arguments);
+			var sections = uci.sections('eqos', 'device');
+
+			if (rv !== true)
+				return rv;
+
+			for (var i = 0; i < sections.length; i++) {
+				if (sections[i]['.name'] === section_id)
+					continue;
+
+				if (uci.get('eqos', sections[i]['.name'], 'enabled') === '0')
+					continue;
+
+				if ((uci.get('eqos', sections[i]['.name'], 'queue') ||
+				     uci.get('eqos', sections[i]['.name'], 'comment')) === value)
+					return _('Name already exists.');
+			}
+
+			return true;
+		};
+
+		o = s.option(form.DummyValue, '_match', _('Match'));
+		o.textvalue = function(section_id) {
+			var selector = selectorValue(section_id);
+			var value = matchValue(section_id);
+
+			return value ? '%s: %s'.format(matchLabel(selector), value) : E('em', _('unspecified'));
+		};
+
+		o = s.taboption('general', form.Value, 'download', _('Download speed (kbit/s)'));
+		o.datatype = 'and(uinteger,min(0))';
+		o.rmempty = false;
+		o.editable = true;
+
+		o = s.taboption('general', form.Value, 'upload', _('Upload speed (kbit/s)'));
+		o.datatype = 'and(uinteger,min(0))';
+		o.rmempty = false;
+		o.editable = true;
+
+		o = s.taboption('general', form.ListValue, 'selector', _('Type'));
+		o.modalonly = true;
+		o.default = 'ip';
+		o.rmempty = false;
+		o.value('ip', _('IPv4 address'));
+		o.value('ip6', _('IPv6 address'));
+		o.value('mac', _('MAC address'));
+		o.cfgvalue = function(section_id) {
+			return selectorValue(section_id);
+		};
+		o.write = function(section_id, value) {
+			uci.set('eqos', section_id, 'selector', value);
+
+			if (value !== 'ip')
+				uci.unset('eqos', section_id, 'ip');
+
+			if (value !== 'ip6')
+				uci.unset('eqos', section_id, 'ip6');
+
+			if (value !== 'mac')
+				uci.unset('eqos', section_id, 'mac');
+		};
+
+		o = s.taboption('general', form.Value, 'ip', _('IPv4 address'));
+		o.modalonly = true;
+		o.datatype = 'ip4addr("nomask")';
+		o.placeholder = '192.0.2.100';
+		o.rmempty = false;
+		o.depends('selector', 'ip');
+		addChoices(o, hostChoices.ip);
+
+		o = s.taboption('general', form.Value, 'ip6', _('IPv6 address'));
+		o.modalonly = true;
+		o.datatype = 'ip6addr("nomask")';
+		o.placeholder = '2001:db8::100';
+		o.rmempty = false;
+		o.depends('selector', 'ip6');
+		addChoices(o, hostChoices.ip6);
+
+		o = s.taboption('general', form.Value, 'mac', _('MAC address'));
+		o.modalonly = true;
 		o.datatype = 'macaddr';
-		Object.keys(hosts).forEach(function(mac) {
-			var hint = hosts[mac].name || L.toArray(hosts[mac].ipaddrs || hosts[mac].ipv4)[0];
-			o.value(mac, hint ? '%s (%s)'.format(mac, hint) : mac);
-		});
-		o.rmempty = true;
-
-		o = s.option(form.Value, 'download', _('Download speed (kbit/s)'));
-		o.datatype = 'and(uinteger,min(0))';
+		o.placeholder = '00:11:22:33:44:55';
 		o.rmempty = false;
-
-		o = s.option(form.Value, 'upload', _('Upload speed (kbit/s)'));
-		o.datatype = 'and(uinteger,min(0))';
-		o.rmempty = false;
-
-		o = s.option(form.Value, 'comment', _('Comment'));
-		o.datatype = 'and(uinteger,min(1))';
-		o.rmempty = false;
+		o.depends('selector', 'mac');
+		addChoices(o, hostChoices.mac);
 
 		return m.render();
 	}
